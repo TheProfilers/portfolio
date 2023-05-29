294c5e4339a8c00896f5a5e7e7883e3dbba5d5bb
Brian-mula <mulatiwangilabrian@gmail.com>
2023-05-29 21:40:20 +0300

    modified hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { bwmap, mulati, worldmap } from '../assets';
+import { bwmap, mulati2, worldmap } from '../assets';
 import { styles } from '../styles';
 
 const Hero = () => {
@@ -84,7 +84,7 @@ const Hero = () => {
             className="absolute bottom-0 ml-[50vw] 
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
             sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={mulati}
+            src={mulati2}
             alt="shaquille"
           />
         </div>
